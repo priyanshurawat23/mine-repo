9dfcef35d66b37ea63f9198d65f7fb5b70d7b497
Priyanshu Rawat <priyanshu.r2309@gmail.com>
2025-11-12 10:34:29 +0530

    Add files via upload

---
+++ 2Darray.cpp
@@ -0,0 +1,138 @@
+// h'ola
+
+// // wap to input a 2d array and also print it.
+// // wap to find the addition of 2 matrix/2d array.
+// // wap to find the sum of all no of a matrix.
+// // wAP to find the biggeest no of a matrix.
+// // wap to find the transposwe of a matrix.
+// // // // wap to find the multiplication of two matrix.
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	int a[3][3],i,j;
+// 	for(i=0;i<3;i+=1){
+// 	    for(j=0;j<3;j+=1){
+// 	        cin>>a[i][j];
+	        
+// 	    }
+// 	}
+// 	for(i=0;i<3;i+=1){
+// 	    for(j=0;j<3;j+=1){
+// 	        cout<<a[i][j];
+	        
+// 	    }
+// 	    cout<<"\n";
+	    
+// 	}
+
+// }
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	int a[3][3],b[3][3],i,j;
+// 	for(i=0;i<3;i+=1){
+// 	    for(j=0;j<3;j+=1){
+// 	        cin>>a[i][j];
+	        
+// 	    }
+// 	}
+// 	for(i=0;i<3;i+=1){
+// 	    for(j=0;j<3;j+=1){
+// 	        cin>>b[i][j];
+	        
+// 	    }
+// 	}
+	
+// 	for(i=0;i<3;i+=1){
+// 	    for(j=0;j<3;j+=1){
+// 	        cout<<a[i][j]+b[i][j]<<" ";
+	        
+// 	    }
+// 	    cout<<"\n";
+	    
+// 	}
+
+// }
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	int a[3][3],i,j,p=0;
+// 	for(i=0;i<3;i+=1){
+// 	    for(j=0;j<3;j+=1){
+// 	        cin>>a[i][j];
+	        
+// 	    }
+// 	}
+// 	for(i=0;i<3;i+=1){
+// 	    for(j=0;j<3;j+=1){
+// 	        p+=a[i][j];
+// 	    }
+	    
+// 	}
+// 	cout<<p;
+
+// }
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	int a[3][3],i,j,b;
+// 	for(i=0;i<3;i+=1){
+// 	    for(j=0;j<3;j+=1){
+// 	        cin>>a[i][j];
+	        
+// 	    }
+// 	}
+// 	b=a[1][1];
+// 	for(i=0;i<3;i+=1){
+// 	    for(j=0;j<3;j+=1){
+// 	        if (a[i][j]>b){
+// 	            b=a[i][j];
+// 	        }
+// 	    }
+// 	}
+// 	cout<<b;
+	
+
+// }
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	int a[3][3],i,j;
+// 	for(i=0;i<3;i+=1){
+// 	    for(j=0;j<3;j+=1){
+// 	        cin>>a[i][j];
+	        
+// 	    }
+// 	}
+// 	for(i=0;i<3;i+=1){
+// 	    for(j=0;j<3;j+=1){
+// 	        cout<<a[j][i];
+// 	    }
+// 	    cout<<"\n";
+// 	}
+	
+
+
+// }
